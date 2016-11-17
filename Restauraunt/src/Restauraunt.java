e1a2a67b7adf1f2acbb533d154cbf25140e37c5f
davistj7 <davistj7@miamioh.edu>
2016-11-17 11:13:18 -0500

    added restauraunt object

---
+++ Restauraunt/src/Restauraunt.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Restauraunt {
+	
+	private ArrayList<Employee> employees = new ArrayList<Employee>();
+	private double cost;
+	
+	public Restauraunt() {
+		this(0);
+	}
+	public Restauraunt(double cost) {
+		setBuildingCost(cost);
+	}
+	
+	
+	
+	public double getTotalCosts() {
+		double money = 0;
+		for(Employee e : employees) {
+			if(e instanceof Manager) {
+				
+			}
+		}
+		return money + getBuildingCost();
+	}
+	public double getBuildingCost() {return cost;}
+	public void setBuildingCost(double cost) {this.cost = cost;}
+	
+	
+	
+}
